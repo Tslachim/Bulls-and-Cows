aabaf620f30034d9d01c840671eebc55e4abcda0
Michal Stuchlík <tslachim@gmail.com>
2023-01-09 10:40:35 +0100

    upraveno a přidán měřič času

--- bulls and cows.py
+++ bulls and cows.py
@@ -5,6 +5,7 @@ email: tslachim@gmail.com
 discord: Mišakus#1763
 """
 import random
+from time import time
 
 print("""Hi there!
 -----------------------------------------------
@@ -20,6 +21,7 @@ generated = []
 end = True
 bull = 0
 cow = 0
+attemp = 0
 
 for i in range(0,4):
     num = random.choice(numbers)
@@ -36,7 +38,7 @@ while generated[0] == 0:
     # číslo nesmí začínat 0 
     # číslo nesmí být duplicitní
 print(generated)
-
+start_time = time()
 while end:
     while len(number := input("Enter a four-digit number:\n")) != 4 or not number.isnumeric() or int(number[0]) == 0 or len(set(number)) < 4:
         if len(number) > 4:
@@ -50,6 +52,9 @@ while end:
         elif len(set(number)) != 4:
             print("Numbers must be unique!")
 
+    # počitadlo kolikrát je potřeba zadávat (správné) číslo 
+    attemp += 1
+
     # vytvořit promněné na Cows and Bulls 
     # pokud je uživatelské číslo v generated pak cows, pokud je se stejným indexem tak bull přes funkci for?
 
@@ -60,15 +65,23 @@ while end:
             cow += 1
 
     # Vypiš počet Bull and Cow a pokud to je to množné číslo tak /s      přes funkci if pokud zjistíš jestli je to množné nebo jednotné
-
+    print(20 * "-")
     print(f"{bull} Bull") if bull <= 1 else print(f"{bull} Bulls"), print(f"{cow} Cow") if cow <= 1 else print(f"{cow} Cows")
-    
+    print(20 * "-")
     # v případě uspěšného uhádnutí ukončit cyklus!
 
     if bull == 4:
         end = False
-
+        stop_time = time()
+        
     # vymazat hodnoty bull and cow aby počítání neovlivnilo další kolo 
-
     bull = 0
     cow = 0
+    
+game_time = round(stop_time - start_time, 1)
+
+print(f"""You find all numbers, You win!
+{30 * '='}
+Your time is: {game_time}/s 
+your number of attemps: {attemp} 
+{30 * '='}""")
