dde3941243c07c5620cc8760825bbbf1481956c4
Michal Stuchlík <tslachim@gmail.com>
2023-02-10 13:37:50 +0100

    Upraveno k hraní, upraveno graficky

--- main_B&C2.0.py
+++ main_B&C2.0.py
@@ -4,16 +4,12 @@ from time import time
 from tkinter import *
 from pravidla import open_rules
 
-
-
-# Proměné 
-attemp = 0
-main_font = ("Times New Roman", 15)
-main_color = "#F2C777"
-note = ""
-bull = 0
-cow = 0
-
+# Proměné Barvy
+main_color = "#A66B37"
+dark_brown = "#401201"
+light_color = "#F2C3A7"
+light_color2 = "#F2B47E"
+brown = "#59380E"
 
 # Okno 
 window = Tk()
@@ -34,9 +30,9 @@ text_frame = Frame(window, bg= main_color)
 text_frame.pack()
 
 # Hlavní nadpis - Label
-title = Label(title_frame, text="Vítejte ve hře", bg= main_color, fg="#8C442A", font= ("Arial", 15, "bold"))
+title = Label(title_frame, text="Vítejte ve hře", bg= main_color, fg= dark_brown, font= ("Arial", 10, "bold"))
 title.pack(pady= 5)
-title1 = Label(title_frame, text="Bulls & Cows", bg= main_color, fg="#8C442A", font= ("Arial", 30, "bold"))
+title1 = Label(title_frame, text="Bulls & Cows", bg= main_color, fg= dark_brown, font= ("Arial", 30, "bold"))
 title1.pack(pady= 10)
 
 # Zapis pravidel hry do Labelu s popiskem jak hrát 
@@ -44,23 +40,24 @@ code = """
 Bull/s  = počet uhádnutých čísel, uhádnutých pozic
 Cow/s   = počet uhádnutých čísel, špatných pozic
 """
-info = """Stiskni 'Nová Hra'
-Poté zadej čtyřcifené číslo, které nezačíná 0
-Zároveň se číslice nesmí opakovat"""
+info = """
+Zadej čtyřcifené číslo
+Číslice nesmí začínat 0
+Číslice se za sebou nesmí opakovat(4444)"""
 
 # popis - label
-descri = Label(info_frame, text= code, bg= main_color, fg="#8C442A", font= ("Arial", 8, "italic"))
+descri = Label(info_frame, text= code, bg= main_color, fg=dark_brown, font= ("Arial", 8, "italic"))
 descri.grid(row= 0, column= 0)
 
-description = Label(info_frame, text= code, bg= main_color, fg="#8C442A", font= ("Arial", 8, "italic"))
+description = Label(info_frame, text= info, bg= main_color, fg=dark_brown, font= ("Arial", 8, "italic"))
 description.grid(row= 1, column= 0)
 
 #Scroll bar
-scroll_bar = Scrollbar(text_frame, bg= "#8C442A")
+scroll_bar = Scrollbar(text_frame, bg= light_color)
 scroll_bar.grid(row= 0, column= 1, sticky="NS")
 
 # seznam neuhádnutých čísel a u nich Bull and Cow - textové okno se scrolbarem
-list_box = Listbox(text_frame, width= 31, height= 10, bg= main_color, fg= "#8C442A", font= ("Arial", 8,), yscrollcommand=scroll_bar.set)
+list_box = Listbox(text_frame, width= 40, height= 15, bg= light_color, fg= dark_brown, font= ("Arial", 8,), yscrollcommand=scroll_bar.set)
 list_box.grid(row= 0 ,column= 0)
 
 # Nastavení scroll baru
@@ -70,6 +67,7 @@ scroll_bar.config(command=list_box.yview)
 def new_game():
     """Funkce spustí novou hru
     Vybere čtyřciferné číslo, které nezačíná nulou, kde se neopakuji čislice"""
+    
     list_generated = []
     numbers = list(range(0,10))
     for num in range(0,4): # vygeneruje číso      
@@ -80,30 +78,29 @@ def new_game():
     while list_generated[0] == 0: # pokud je první číslo nula tak jej změní na jiné.
         list_generated[0] = random.choice(numbers)
     list_box.delete(0, END)
-    print(list_generated)
+    # print(list_generated) # --->> pro případné ověřování je potřeba odkomentovat tento print
     return list_generated
-
 new_game_value = new_game()
 
 # New game - button, Exit - button
-new_g = Button(info_frame, text="Nová hra", width=20, bg= "#8C442A", fg= main_color, font= ("Arial", 10, "bold"), command= new_game)
+new_g = Button(info_frame, text="Nová hra", width=20, bg= dark_brown, fg= light_color2, font= ("Arial", 10, "bold"), command= new_game)
 new_g.grid(row=1, column=1, padx= 5, pady=10)
 
 # tlačítko na pravidla hry 
-rules = Button(info_frame, text= "Pravidla hry", bg= "#8C442A", fg= main_color, font= ("Arial", 8, "italic"), command= open_rules)
-rules.grid(row= 0, column= 1, padx=30, pady= 20, ipadx= 5, ipady= 5)
+rules = Button(info_frame, width= 20, text= "Pravidla hry", bg= dark_brown, fg= light_color2, font= ("Arial", 10, "bold"), command= open_rules)
+rules.grid(row= 0, column= 1, padx=10, pady= 20)
 
 # Hádané číslo (input)  # místo pro zapsání že číslo je dlouhé, krátké, začíná nulou .... a Bull and Cows
-user_input = Entry(input_frame, text= "0000", width= 8)
+user_input = Entry(input_frame, text= "0000", width= 8, bg=light_color, fg=dark_brown)
 user_input.grid(row= 0, column= 0, padx= 15)
 
 # aktuální Bull and Cow případně chyba zadání
-output = Label(input_frame, bg= main_color, fg="#8C442A", font= ("Arial", 8, "italic"))
+output = Label(input_frame, bg= main_color, fg=brown, font= ("Arial", 12, "bold"))
 output.grid(row= 0, column=2, padx=5)
 
 #Varování při chybě
-output1 = Label(input_frame, bg= main_color, fg="#8C442A", font= ("Arial", 12, "bold"))
-output1.grid(row= 1, column=1, padx=5, pady=10)
+output1 = Label(input_frame, bg= main_color, fg=dark_brown, font= ("Arial", 12, "bold"))
+output1.grid(row= 1, column=2, padx=10, pady=10)
 
 number = user_input.get()
 
@@ -113,7 +110,7 @@ def bull_cow(bull, cow):
     if bull == 4:
         stop_time = time()
         note = f"{user_number} = 4 Bulls, 0 Cow"
-        end_label.config(text= "Uhádl jsi všechny čtyři čísla!")
+        end_label.config(text= f"Uhádl jsi všechny čísla!\n {user_number}\n Stiskni 'Nová hra' \na dej si to ještě jednou!")
     elif bull > 1 and cow <= 1:
         note = f"{user_number} = {bull} Bulls, {cow} Cow."
         output.config(text= note)
@@ -164,11 +161,11 @@ def evaulation():
     bull_cow(bull, cow) 
 
 # tlačítko pro vyhodnocení 
-correct_button = Button(input_frame, text="Vyhodnoť číslo", width=20, bg= "#8C442A", fg= main_color, font= ("Arial", 8, "bold"), command= evaulation)
+correct_button = Button(input_frame, text="Vyhodnoť číslo", width=20, bg= dark_brown, fg= light_color2, font= ("Arial", 8, "bold"), command= evaulation)
 correct_button.grid(row=0, column=1, padx= 5, pady=20)
 
 # na konci výpis konec hry, čas hry, počet pokusů, nejlepší počet pokusu - potřeba uložit do txt.
-end_label = Label(text_frame, font= ("Arial", 10, "bold"), bg= main_color, fg="#8C442A")
+end_label = Label(text_frame, font= ("Arial", 15, "bold"), bg= main_color, fg=dark_brown)
 end_label.grid(row=0, column= 2)
 
 window.mainloop()
--- pravidla.py
+++ pravidla.py
@@ -1,6 +1,8 @@
 from tkinter import Tk,Label
 
 main_color = "#F2C777"
+dark_brown = "#401201"
+
 rules_note_1 = """
 Uhádnout čtyřciferné číslo ve správném pořadí\n
 Máš neomezený počet pokusů\n
@@ -32,22 +34,22 @@ def open_rules():
     w_rules.iconbitmap("bull.ico")
     w_rules.config(bg=main_color)
 
-    rules_title = Label(w_rules, relief="raised", borderwidth=3, text= "Pravidla hry Bulls and Cows\n Začni stisknutím nové hry(spustíš čas)", bg= main_color, fg="Black", font= ("Arial", 15, "bold"))
-    rules_title.grid(row=0, column=1, pady=10)
+    rules_title = Label(w_rules, text= "Pravidla hry Bulls and Cows", bg= main_color, fg=dark_brown, font= ("Arial", 25, "bold"))
+    rules_title.grid(row= 0, column= 1, pady=15, padx=15)
 
-    rules_title_1 = Label(w_rules, relief="raised", anchor="nw", borderwidth=3, text= "Cíl hry:", bg= main_color, fg="Black", font= ("Arial", 15, "bold"))
+    rules_title_1 = Label(w_rules, relief="raised", anchor="w", borderwidth=3, text= "Cíl hry:", bg= main_color, fg=dark_brown, font= ("Arial", 15, "bold"))
     rules_title_1.grid(row=1, column=0, padx=30)
-    rules_text_1 = Label(w_rules, text= rules_note_1, anchor="w", bg= main_color, fg="Black", font= ("Arial", 12, "bold"))
+    rules_text_1 = Label(w_rules, text= rules_note_1, anchor="w", bg= main_color, fg=dark_brown, font= ("Arial", 12, "bold"))
     rules_text_1.grid(row=1, column=1, pady=10)
 
-    rules_title_2 = Label(w_rules, relief="raised", anchor="nw", borderwidth=3, text= "Vysvětlení Cow/s:", bg= main_color, fg="Black", font= ("Arial", 15, "bold"))
+    rules_title_2 = Label(w_rules, relief="raised", anchor="w", borderwidth=3, text= "Vysvětlení Cow/s:", bg= main_color, fg=dark_brown, font= ("Arial", 15, "bold"))
     rules_title_2.grid(row=2, column=0, padx=30)
-    rules_text_2 = Label(w_rules, text= rules_note_2, anchor="w", bg= main_color, fg="Black", font= ("Arial", 12, "bold"))
+    rules_text_2 = Label(w_rules, text= rules_note_2, anchor="w", bg= main_color, fg=dark_brown, font= ("Arial", 12, "bold"))
     rules_text_2.grid(row=2, column=1, pady=10)
 
-    rules_title_3 = Label(w_rules, relief="raised", anchor="nw", borderwidth=3, text= "Vysvětlená Bull/s", bg= main_color, fg="Black", font= ("Arial", 15, "bold"))
+    rules_title_3 = Label(w_rules, relief="raised", anchor="w", borderwidth=3, text= "Vysvětlená Bull/s", bg= main_color, fg=dark_brown, font= ("Arial", 15, "bold"))
     rules_title_3.grid(row=3, column=0, padx=30)
-    rules_text_3 = Label(w_rules, text= rules_note_3, anchor="w", bg= main_color, fg="Black", font= ("Arial", 12, "bold"))
+    rules_text_3 = Label(w_rules, text= rules_note_3, anchor="w", bg= main_color, fg=dark_brown, font= ("Arial", 12, "bold"))
     rules_text_3.grid(row=3, column=1, pady=10)
 
 
@@ -55,4 +57,4 @@ def open_rules():
 
 
 
-# w_rules.mainloop() # tohle nebudu možná potřebuovat při volání funkce Pravidel hry 
+# w_rules.mainloop() # tohle nebudu potřebuovat při volání funkce Pravidel hry 
